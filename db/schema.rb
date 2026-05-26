5ae0d8f251d4c01d827a746856d684d73cdcecfd
Sarthak <sarthak.bhosale@definelabs.com>
2026-05-27 02:07:34 +0530

    user validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_26_153512) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_26_202522) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
